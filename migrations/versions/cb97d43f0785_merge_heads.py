4c1f6b2d491a2e2d09cfdba6e0f1c481d52bbdd5
citybeachgulf-droid <citybeachgulf@gmail.com>
2025-10-13 17:00:16 +0400

    20

---
+++ migrations/versions/cb97d43f0785_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: cb97d43f0785
+Revises: 7b1e8d8b3d9a, 86b165564759
+Create Date: 2025-10-13 16:40:12.034004
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cb97d43f0785'
+down_revision = ('7b1e8d8b3d9a', '86b165564759')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
